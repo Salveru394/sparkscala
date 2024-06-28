f2cb5e7d6e37153acadb958e28600f60b4021bf5
91957 <rahulsalveru100@gmail.com>
2024-06-28 22:47:37 +0530

    The New tenth.scala class

---
+++ src/main/scala/tenth.scala
@@ -0,0 +1,11 @@
+object tenth {
+def main(args:Array[String]):Unit ={
+  val number = -8
+
+  if (number >= 0 || number %2 ==0){
+    print("number is either non-negative or even")
+  }else{
+    print("number is neither non-negative nor even")
+  }
+}
+}
