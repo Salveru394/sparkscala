2a8c8bfa6e8ddf4353ebbdc15676b8c50d96526a
91957 <rahulsalveru100@gmail.com>
2024-06-28 22:39:29 +0530

    The New sixth.scala class

---
+++ src/main/scala/sixth.scala
@@ -0,0 +1,10 @@
+object sixth {
+def main(args: Array[String]):Unit ={
+  val number = -7
+  if (number < 0 && number % 2 !=0){
+    println("number is both negative and odd")
+  }else{
+    println("number is not both negative and odd")
+  }
+}
+}
