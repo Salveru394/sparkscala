c0c2ad1190b7971f426966651c3705bb043fbaec
91957 <rahulsalveru100@gmail.com>
2024-06-28 22:39:58 +0530

    The New nineth.scala class

---
+++ src/main/scala/nineth.scala
@@ -0,0 +1,12 @@
+object nineth {
+def main(args:Array[String]):Unit ={
+
+  var number = 35
+
+  if (number % 5 == 0 && number % 7 == 0){
+    println("number is divisible by both 5 and 7")
+  }else{
+    println("number is not divisible by both 5 and 7")
+  }
+}
+}
