d720c934f021e630c6a2d0a22f47fec703628a92
91957 <rahulsalveru100@gmail.com>
2024-06-28 23:31:05 +0530

    The New twelfth.scala class

---
+++ src/main/scala/twelfth.scala
@@ -0,0 +1,15 @@
+object twelfth {
+def main(args: Array[String]):Unit ={
+
+  var purchase =120
+  purchase > 150
+  purchase > 100
+
+  if (purchase > 150 || purchase > 100){
+    println("you are eligible for freeshipping ")
+  }else{
+    println("you are not eligible  freeshipping")
+  }
+}
+}
+
